43707d816a6eae94d9215851c8d0e5bb48fee2b1
Drew Elgar <drew_elgar@outlook.com>
2026-06-01 12:32:43 +1000

    Leader keybinds.

--- init.lua
+++ init.lua
@@ -95,8 +95,8 @@ do
   -- Set <space> as the leader key
   -- See `:help mapleader`
   --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-  vim.g.mapleader = '<space>'
-  vim.g.maplocalleader = '<space>'
+  vim.g.mapleader = ' '
+  vim.g.maplocalleader = ' '
 
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = true
